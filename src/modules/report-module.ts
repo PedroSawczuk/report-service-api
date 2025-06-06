86b0bdb7cdd70f24a703854c5e1779cc679d5bc1
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-06-06 14:39:48 -0400

    feat: add GET endpoints for retrieving all reports and a specific report by ID

--- src/modules/report-module.ts
+++ src/modules/report-module.ts
@@ -3,6 +3,27 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function reportModule(app: FastifyInstance) {
+  app.get('/', async () => {
+    const reports = await knex('reports').select('*')
+    return reports
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getReportParamsSchema = z.object({
+      id: z.string().uuid('ID inválido'),
+    })
+
+    const { id } = getReportParamsSchema.parse(request.params)
+
+    const report = await knex('reports').where({ id }).first()
+
+    if (!report) {
+      return reply.status(404).send('Relatório não encontrado')
+    }
+
+    return report
+  })
+
   app.post('/', async (request, reply) => {
     const createReportBodySchema = z.object({
       title: z.string().min(1, 'Título é obrigatório'),
@@ -21,12 +42,13 @@ export async function reportModule(app: FastifyInstance) {
       url_path: urlPath,
     })
 
-    return reply
-      .status(201)
-      .send(
-        `Relatório criado com sucesso! \n\n Título: ${title} \n\n Descrição: ${
-          description || 'Nenhuma descrição fornecida'
-        } \n\n URL: ${urlPath || 'Nenhuma URL fornecida'}`,
-      )
+    return reply.status(201).send(
+      `Relatório criado com sucesso! \n\n 
+        Título: ${title} 
+          \n\n 
+        Descrição: ${description || 'Nenhuma descrição fornecida'} 
+          \n\n 
+        URL: ${urlPath || 'Nenhuma URL fornecida'}`,
+    )
   })
 }
